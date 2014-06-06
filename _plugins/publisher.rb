2e561c5045257ee3b7bc44664203e489b9378fc9
Blaine Pace <blainepace@gmail.com>
2014-06-06 01:19:07 -0700

    Add PostPublisher Jekyll Plugin
    Added a jekyll plugin called PostPublisher. 99% of code comes from a blog post
    over at [Jeffrey Sambells][jsambells] blog. The code as-is was crashing when
    "date: unpublished" existed in the yaml header in unpublished posts, so some
    tweaks were made. The plugin now yanks out the yaml header and injects a date
    field (overwriting it if it exists) instead of assuming the date field exists
    and gsubbing it.
    
    [jsambells]: http://http://jeffreysambells.com/2013/02/01/jekyll-draft-publishing-plugin/

---
+++ _plugins/publisher.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+require 'psych'
+
+module Jekyll
+  class PostPublisher < Generator
+    safe true
+
+    def replace(filepath, regex, *args, &block)
+      content = File.read(filepath).gsub(regex, *args, &block)
+      File.open(filepath, 'wb') { |file| file.write(content) }
+    end
+
+    def generate(site)
+      @files = Dir["_publish/*"]
+      @files.each_with_index do |f,i|
+        now = DateTime.now
+        replace(f, /---(.|\n)*---/) do |match|
+          header = Psych.load(match)
+          header["date"] = now.strftime("%Y-%m-%d %H:%M:%S")
+          "#{header.to_yaml}---"
+        end
+        today = now.strftime("%Y-%m-%d")
+        File.rename(f, "_posts/#{today}-#{File.basename(f)}")
+      end
+    end
+  end
+end
